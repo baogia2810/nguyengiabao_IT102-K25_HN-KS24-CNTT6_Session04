6c62841e92139cc99fb7a3a26527c9caa2e5b802
baogia2810 <bankaijun2880@gmail.com>
2025-07-30 22:19:04 +0700

    upload bài 4

---
+++ IT102-K25_HN-KS24-CNTT6_Session04_B4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int month;
+    printf("Nhap so thang: ");
+    scanf("%d", &month);
+    switch (month) {
+        case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+            printf("Thang %d co 31 ngay\n", month);
+            break;
+        case 4: case 6: case 9: case 11:
+            printf("Thang %d co 30 ngay\n", month);
+            break;
+        case 2:
+            printf("Thang %d co 28 hoac 29 ngay\n", month);
+            break;
+        default:
+            printf("So thang khong hop le\n");
+    }
+    return 0;
+}
