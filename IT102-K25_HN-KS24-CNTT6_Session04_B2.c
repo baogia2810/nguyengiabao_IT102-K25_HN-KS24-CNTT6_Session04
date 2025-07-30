65bca4c85ed3d6a84a5755734cf34e0a92d8edba
baogia2810 <bankaijun2880@gmail.com>
2025-07-30 22:10:53 +0700

    upload bài 2

---
+++ IT102-K25_HN-KS24-CNTT6_Session04_B2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Nhap so nguyen: ");
+    scanf("%d", &n);
+    if (n % 2 == 0) {
+        printf("%d la so chan\n", n);
+    } else {
+        printf("%d la so le\n", n);
+    }
+    return 0;
+}
